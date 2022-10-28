a017d2977d65bcd20fe666349290b0e28ec39322
i-havr <i.havrylov@gmail.com>
2022-10-28 13:50:34 +0300

    done task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,23 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const currentStorageData = localStorage.getItem(STORAGE_KEY);
+
+setTimeAfterPageRefresh();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime.seconds));
+}
+
+function setTimeAfterPageRefresh() {
+  if (currentStorageData) {
+    player.setCurrentTime(currentStorageData);
+    console.log(iframe);
+  }
+}
